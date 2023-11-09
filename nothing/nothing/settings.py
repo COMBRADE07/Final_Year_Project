5399f6478a388b07702264313aea95cfb2609175
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2023-11-09 18:59:38 +0530

    Update settings.py

--- nothing/nothing/settings.py
+++ nothing/nothing/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'nothing.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nothing',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSSWORD': '',
     }
 }
 
